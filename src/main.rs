f08a8300c558dc4bb79e7d880a7aacd736345a2c
John Teske <john.teske@protonmail.com>
2020-10-04 22:47:48 -0700

    Tidy up, add new types

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,27 @@ extern crate termion;
 use termion::raw::IntoRawMode;
 use termion::{clear, cursor};
 
-use std::io::{self, Write};
+use std::io::{stdout, Write};
 use std::{thread, time};
 
+enum Material {
+    Sand,
+}
+
+struct Pixel {
+    position: usize,
+    material: Material,
+}
+
 fn main() {
-    let stdout = io::stdout();
+    let stdout = stdout();
     let stdout = stdout.lock();
     let mut stdout = stdout.into_raw_mode().unwrap();
 
-    // let termsize = termion::terminal_size().ok();
-    // let termwidth = termsize.map(|(w,_)| w - 2);
-    // let termheight = termsize.map(|(_,h)| h - 2);
+    let (width, height) = terminal_width_height();
 
     const BLOCK: &'static str = "▒";
-    let max_frames = 30;
+    let max_frames = width; // TODO for demo purposes only
     let delay = time::Duration::from_millis(33);
 
     write!(stdout, "{}{}", clear::All, cursor::Hide).unwrap();
@@ -28,14 +35,17 @@ fn main() {
 
         // increment
         stdout.flush().unwrap();
-        count += 1;
         thread::sleep(delay);
+        count += 1;
         if count >= max_frames {
             break;
         }
     }
 
-    write!(stdout, "{}", termion::cursor::Show)
-        //write!(stdout, "{}{}", termion::clear::All, termion::cursor::Show)
+    write!(stdout, "{}{}", termion::clear::All, termion::cursor::Show)
         .expect("Unable to restore terminal");
+}
+
+fn terminal_width_height() -> (u16, u16) {
+    termion::terminal_size().expect("Unable to get terminal size")
 }
