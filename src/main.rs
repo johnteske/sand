a660dbd8f6e9eeaa994ef8740f4e87b3153efcb9
John Teske <john.teske@protonmail.com>
2020-10-10 21:20:05 -0700

    Handle SW/SE settling every other frame

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate termion;
 
+use std::f64::consts::PI;
+
+use rand::prelude::*;
 use termion::raw::IntoRawMode;
 use termion::{clear, cursor};
 
@@ -17,8 +20,11 @@ impl Point {
     }
 }
 
-fn drop_or_settle(point: &Point, points_below: Vec<bool>) -> Point {
+fn drop_or_settle(point: &Point, points_below: Vec<bool>, frame: u16) -> Point {
+    let is_even = (frame & 1) == 0;
+
     // drop
+    // s
     if points_below[1] {
         return Point {
             x: point.x,
@@ -27,8 +33,29 @@ fn drop_or_settle(point: &Point, points_below: Vec<bool>) -> Point {
     }
 
     // settle
-    if points_below[2] {
+    if points_below[0] && points_below[2] {
+        if is_even {
+            // sw
+            return Point {
+                x: point.x - 1,
+                y: point.y + 1,
+            };
+        }
         // se
+        return Point {
+            x: point.x + 1,
+            y: point.y + 1,
+        };
+    }
+    // sw
+    if points_below[0] {
+        return Point {
+            x: point.x - 1,
+            y: point.y + 1,
+        };
+    }
+    // se
+    if points_below[2] {
         return Point {
             x: point.x + 1,
             y: point.y + 1,
@@ -50,7 +77,7 @@ fn main() {
     let (width, height) = terminal_width_height();
     let area = width * height;
 
-    const FPS: u64 = 15;
+    const FPS: u64 = 8;
     let delay = time::Duration::from_millis(1000 / FPS);
     let max_frames = height;
     let mut frames = 0;
@@ -61,21 +88,32 @@ fn main() {
     let mut vec: Vec<Point> = Vec::with_capacity(area as usize);
     // TODO add a single-cell border around the screen to avoid the need to boundary-check
     // --which would also help the coordinates of termion being 1,1-origin
-    vec.push(Point::new(1, 1));
-    vec.push(Point::new(3, 1));
-    vec.push(Point::new(1, 3));
+
+    // populate screen
+    // TODO this looks like it streams from the center of the screen
+    let mut rng = rand::thread_rng();
+    for _ in 1..99 {
+        let a = rng.gen::<f64>() * 2.0 * PI;
+        let r = ((height / 3) as f64) * (rng.gen::<f64>()).sqrt();
+        let x: u16 = (r * a.cos()) as u16;
+        let y: u16 = (r * a.sin()) as u16;
+        // do not add point if exists already
+        if vec.iter().position(|r| r.x == x && r.y == y).is_none() {
+            vec.push(Point::new(x + (width / 2), y + (height / 2)));
+        }
+    }
 
     loop {
         if frames >= max_frames {
             break;
         }
 
-        // clear screen between frames
-        write!(stdout, "{}{}", clear::All, cursor::Hide).unwrap();
-
         // TODO sort by y
         for i in 0..vec.len() {
-            // TODO check boundaries
+            // erase glyph before move
+            // TODO only erase if moving
+            write!(stdout, "{}{}", cursor::Goto(vec[i].x, vec[i].y), " ").unwrap();
+
             vec[i] = drop_or_settle(
                 &vec[i],
                 vec![
@@ -100,10 +138,11 @@ fn main() {
                             .position(|r| r.x == vec[i].x + 1 && r.y == vec[i].y + 1)
                             .is_none(),
                 ],
+                frames,
             );
 
             // write glyph
-            write!(stdout, "{}{}", cursor::Goto(vec[i].x, vec[i].y), "#").unwrap();
+            write!(stdout, "{}{}", cursor::Goto(vec[i].x, vec[i].y), "■").unwrap();
         }
 
         // write to stdout
