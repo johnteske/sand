778f02e33f598757c757681c1332740dc14167c0
John Teske <john.teske@protonmail.com>
2020-11-04 20:55:14 -0800

    Add termion adapter

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use termion::{clear, cursor, style};
 
-use std::io::{stdin, stdout, Write};
+use std::io::{stdin, stdout};
 use std::{thread, time};
 
 mod material;
 use material::{Material, Sand};
 
+mod term;
+
 fn main() {
     let stdout = stdout();
     let stdout = stdout.lock();
-    let mut stdout = stdout.into_raw_mode().unwrap();
+    let stdout = stdout.into_raw_mode().unwrap();
+
     let stdin = stdin();
+
+    let mut t = term::Term::new(stdout);
 
     let (width, height) = terminal_width_height();
     let area = width * height;
@@ -23,7 +27,7 @@ fn main() {
     let max_frames = 99;
     let mut frames = 0;
 
-    write!(stdout, "{}{}", clear::All, cursor::Hide).unwrap();
+    t.clear();
 
     // array of materials
     let mut vec: Vec<Material> = Vec::with_capacity(area as usize);
@@ -43,7 +47,7 @@ fn main() {
         moves += 0;
 
         // write to stdout
-        stdout.flush().unwrap();
+        t.flush().unwrap();
 
         // wait
         thread::sleep(delay);
@@ -52,24 +56,17 @@ fn main() {
         frames += 1;
     }
 
-    write!(stdout, "{}", style::Reset).unwrap();
+    t.reset();
 
     for c in stdin.keys() {
         match c.unwrap() {
             Key::Char('q') => break,
-            _ => write!(stdout, "{}Press 'q' to exit", cursor::Goto(1, 1),).unwrap(),
+            _ => t.wr("Press 'q' to exit"),
         }
-        stdout.flush().unwrap();
+        t.flush().unwrap();
     }
 
-    write!(
-        stdout,
-        "{}{}{}",
-        termion::clear::All,
-        cursor::Goto(1, 1),
-        termion::cursor::Show
-    )
-    .expect("Unable to restore terminal");
+    t.restore();
 }
 
 fn terminal_width_height() -> (u16, u16) {
---
+++ src/term.rs
@@ -0,0 +1,62 @@
+// inspiration from:
+// https://github.com/fdehau/tui-rs/blob/master/src/backend/termion.rs
+
+use std::io::Write;
+use termion::{clear, cursor, style};
+
+pub struct Term<W>
+where
+    W: Write,
+{
+    stdout: W,
+}
+
+impl<W> Term<W>
+where
+    W: Write,
+{
+    pub fn new(stdout: W) -> Term<W> {
+        Term { stdout }
+    }
+
+    pub fn clear(&mut self) {
+        write!(self.stdout, "{}{}", clear::All, cursor::Hide).unwrap();
+    }
+
+    pub fn reset(&mut self) {
+        write!(self.stdout, "{}", style::Reset).unwrap();
+    }
+
+    pub fn restore(&mut self) {
+        write!(
+            self.stdout,
+            "{}{}{}",
+            termion::clear::All,
+            cursor::Goto(1, 1),
+            termion::cursor::Show
+        )
+        .expect("Unable to restore terminal");
+    }
+
+    // should this be private?
+    pub fn flush(&mut self) -> std::io::Result<()> {
+        self.stdout.flush()
+    }
+    pub fn wr(&mut self, s: &str) {
+        write!(self.stdout, "{}{}", s, cursor::Goto(1, 1)).unwrap();
+    }
+}
+
+//impl<W> Write for Term<W>
+//where
+//    W: Write,
+//{
+//    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+//                self.stdout.write(buf)
+//                    }
+//
+//    fn flush(&mut self) -> std::io::Result<()> {
+//                self.stdout.flush()
+//                    }
+//
+//}
